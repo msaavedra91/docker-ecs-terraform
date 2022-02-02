3f3c10eab4d534898f405cd495c386e999919eac
Miguel Saavedra <miguel.saavedra@6connex.com>
2022-02-02 17:11:41 -0500

    modify ecs and alb

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -45,6 +45,12 @@ resource "aws_ecs_service" "ecs_service_workshop" {
     launch_type     = "FARGATE"
     desired_count   = 2
 
+    load_balancer {
+        target_group_arn = "${aws_lb_target_group.target_group.arn}"
+        container_name   = "${aws_ecs_task_definition.ecs_task_workshop.family}"
+        container_port   = 5637
+    }
+
     network_configuration {
         subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
         assign_public_ip = true
